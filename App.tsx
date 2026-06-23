77205f1c90b9a30606352b8bdedd64250ef93d82
vitto2099 <vck987123@gmail.com>
2026-06-23 09:31:41 -0300

    UI: remover marca d'água global do rodapé

--- App.tsx
+++ App.tsx
@@ -1,26 +1,7 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <AppNavigator />
-      <View style={styles.watermarkContainer} pointerEvents="none">
-        <Image source={require('./assets/logo.png')} style={styles.watermarkLogo} />
-      </View>
-    </View>
-  );
+  return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1 },
-  watermarkContainer: {
-    position: 'absolute',
-    bottom: 30,
-    alignSelf: 'center',
-    opacity: 0.15,
-  },
-  watermarkLogo: { width: 60, height: 60, resizeMode: 'contain' }
-});
 
