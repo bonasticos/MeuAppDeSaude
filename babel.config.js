567f01213840e57564ed6f0e661dd1bb7de9e8ce
vitto2099 <vck987123@gmail.com>
2026-06-25 11:05:16 -0300

    fix: resolve web bundle white screen by fixing babel and downgrading zustand

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,16 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      function () {
+        return {
+          visitor: {
+            MetaProperty(path) {
+              path.replaceWithSourceString('process');
+            },
+          },
+        };
+      },
+    ],
   };
 };
